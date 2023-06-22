0f88629f9f151537774e6e6d5f4456e83ee28484
Kristian Lein-Mathisen <kristianlein@gmail.com>
2023-06-22 22:03:41 +0200

    gdb feat: adds flash memory mapping
    now gdb can tell me this:
    
       (gdb) i mem
       Using memory regions provided by the target.
       Num Enb Low Addr   High Addr  Attrs
       0   y  	0x00000000 0x00800000 flash blocksize 0x40 nocache
       1   y  	0x00800000 0x01000000 rw nocache
    
    and we got logs like this:
    
        » "vFlashErase:00000000,00000080"
        «  "$OK#9a"
         » "vFlashWrite:0:\x19 \x1f…
        «  "$OK#9a"
         » "vFlashWrite:30:\t\b\…
        «  "$OK#9a"
         » "vFlashDone"
        flash: skipping page #x40
        flash: skipping page #x0

--- gdb.scm
+++ gdb.scm
@@ -42,10 +42,10 @@
                          (string->list
                           (memory-read* (gdb-adr->updi-adr addr) len)))))
                  (lambda (addr vec)
-                   (print "WRITING " addr " " (vector-length vec))
+                   (print "\x1b[32m" "flash" "\x1b[0m: writing page #x" (number->string addr 16))
                    (let ((bytes
                           (list->string (map integer->char (vector->list vec)))))
-                    (memory-write* (gdb-adr->updi-adr addr) bytes 1))
+                     (memory-write* (gdb-adr->updi-adr addr) bytes 1))
                    ;;          ,-- erase and program
                    (STS #x1000 3 1)))
     (cache #:clear)))
@@ -66,6 +66,7 @@
 
 ;; https://sourceware.org/gdb/onlinedocs/gdb/Overview.html#Overview
 ;; "$" packet "#" checksum
+;; and https://sourceware.org/gdb/onlinedocs/gdb/Overview.html#Binary-Data
 (define (rsp-read-packet ip)
   (let loop ((packet '()))
     (let ((c (read-char* ip)))
@@ -73,6 +74,9 @@
             ((eq? c #\#)
              (rsp-read-checksum ip)
              (list->string (reverse packet)))
+            ((eq? c #\}) ;; <-- next byte is escaped
+             (loop (cons (integer->char (bitwise-xor #x20 (char->integer (read-char* ip))))
+                         packet)))
             (else (loop (cons c packet)))))))
 
 (define (packet? cmd) (string? cmd))
@@ -154,12 +158,17 @@
     (stop!)
     (rsp-write "S00" op))
 
+   ;; from my avr-gdb:
+   ;; "qSupported:multiprocess+;swbreak+;hwbreak+;…"
+   ((irregex-match `(: "qSupported:" (* any)) cmd)
+    => (lambda (m)
+         (rsp-write "qXfer:memory-map:read+;" op)))
+
    ((equal? cmd "s")
     (step!) ;;   ,-- this is a magic, undocumented, number
     (rsp-write "S05" op))
 
    ((equal? cmd "c")
-    (cache-flush!)
     (cont!)
 
     ;; the target doesn't tell us when it's stopped. we have to keep
@@ -194,19 +203,15 @@
    ;; TODO: Strictly speaking, we should (cont!). But it's nice to use
    ;; the repl and GDB concurrently, and this is less intrusive.
    ((equal? cmd "D")
-    (cache-flush!)
     (rsp-write "OK" op))
 
    ((equal? cmd "qAttached") ;; attached to existing "process"?
-    (cache-flush!)
     (rsp-write "1" op))
 
    ((equal? cmd "?") ;; stop reason
-    (cache-flush!)
     (rsp-write "S00" op))
 
    ((equal? cmd "g") ;; send all registers
-    (cache-flush!)
     (rsp-write (string->hex
                 (->bytes
                  (regs) ;; r0 - r31
@@ -233,7 +238,6 @@
                         (bytes->u8 blob)
                         (bytes->u16le blob))))
         (print "R = " R " "(wrt value))
-        (cache-flush!)
         (cond ((= R #x22) (set! (PC) value))
               ((= R #x21) (set! (SP) value))
               (else (set! (r R) value)))
@@ -244,7 +248,6 @@
    ;; "xxXXxxXX" hex string => normal output
    ;; "E xx" => error
    ((and (string? cmd) (string-prefix? cmd "qRcmd,"))
-    (cache-flush!)
     (apply
      (lambda (_ #!optional (hex ""))
        (rsp-write
@@ -259,7 +262,6 @@
     => (lambda (m)
          (let* ((addr (string->number (irregex-match-substring m 'addr) 16))
                 (len  (string->number (irregex-match-substring m 'len) 16)))
-           (cache-flush!)
            (rsp-write (string->hex (memory-read* (gdb-adr->updi-adr addr) len)) op))))
 
    ;; TODO: support X for less hex parsing and stuff maybe
@@ -313,6 +315,71 @@
                   (rsp-write "OK" op))
                  ;; type 2, 3, 4: watchpoints (read, write, access)
                  (else (rsp-write "" op))))))
+
+   ;; ==================== flash and memory ====================
+   ;;
+   ;; This is a lot of hassle and we could get by without it: the
+   ;; default fixed memory mapping (program/code splits at 0x800000)
+   ;; is ok. It has one problem however: after "load file.elf", we get
+   ;; a lot of arbitrary write memory, "M…", packets, and we don't
+   ;; know when we should cache-flush!. By telling gdb about our flash
+   ;; region, we get a documented vFlashDone which is exactly what we
+   ;; want. This lets us cache flash pages and save many, probably
+   ;; very many, erase-write cycles: I've seen a lot of "skipping
+   ;; identical flash page" in my logs. The following packets are what
+   ;; seems to be needed just to get our vFlashDone notification.
+
+   ;; request memory map, eg. "qXfer:memory-map:read::0,18a"
+   ;;                          ,-- object     ,- annex must be empty
+   ((irregex-match `(: "qXfer:memory-map:read:"
+                       ":" (=> offset (* xdigit))
+                       "," (=> length (* xdigit))) cmd)
+    => (lambda (m)
+         (let* ((offset (string->number (irregex-match-substring m 'offset) 16))
+                (length (string->number (irregex-match-substring m 'length) 16))
+                (blob "<?xml version=\"1.0\"?>
+<!DOCTYPE memory-map
+          PUBLIC \"+//IDN gnu.org//DTD GDB Memory Map V1.0//EN\"
+                 \"http://sourceware.org/gdb/gdb-memory-map.dtd\">
+<memory-map>
+  <memory type=\"ram\"   start=\"0x800000\" length=\"0x800000\"/>
+  <memory type=\"flash\" start=\"0x000000\" length=\"0x800000\">
+    <property name=\"blocksize\">0x40</property>
+  </memory>
+</memory-map>")
+                (end (min (string-length blob) (+ offset length))))
+           (rsp-write (conc (if (>= end (string-length blob)) "l" "m")
+                            ;; TODO:    ,-- escape non-hex binary response
+                            (substring blob offset end)) op))))
+
+   ;; "vFlashErase:00000000,00000080"
+   ((irregex-match `(: "vFlashErase"
+                       ":" (=> addr (* xdigit))
+                       "," (=> len  (* xdigit))) cmd)
+    => (lambda (m)
+         (let* ((addr (string->number (irregex-match-substring m 'addr) 16))
+                (len  (string->number (irregex-match-substring m 'len) 16)))
+           (memory-write addr (make-string len #\xff))
+           (rsp-write "OK" op))))
+
+   ;; "vFlashWrite:0:\x19 \x1f\x1e\x1d\x1c\x1b\x1a\x19\x18\x17…"
+   ((irregex-match `(: "vFlashWrite"
+                       ":" (=> addr (* xdigit))
+                       ":" (=> blob (* any))) cmd)
+    => (lambda (m)
+         (let* ((addr (string->number (irregex-match-substring m 'addr) 16))
+                (blob                 (irregex-match-substring m 'blob)))
+           (memory-write addr blob)
+           (rsp-write "OK" op))))
+
+   ;; We are permitted to keep flash in an "unpredictable" state until
+   ;; gdb issues this. It should therefore be the only place that
+   ;; needs to flush the flash cache.
+   ((irregex-match `(: "vFlashDone") cmd)
+    => (lambda (m)
+         (let* ()
+           (cache-flush!)
+           (rsp-write "OK" op))))
 
    ;; empty packet reply for unknown commands (of which there are
    ;; _a lot_.
